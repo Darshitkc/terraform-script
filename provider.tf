a2b7215a29e449089437e6032db141aa712af649
Darshit KC <147159561+Darshitkc@users.noreply.github.com>
2024-02-25 22:37:15 +0545

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region= "us-east-1"
-  access_key = "AKIAZ2DTJ3KLH74HVRU4"
-  secret_key = "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"
+  access_key = "AKIAZ2DTJ3KLNHBSQZOY"
+  secret_key = "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"
 }
